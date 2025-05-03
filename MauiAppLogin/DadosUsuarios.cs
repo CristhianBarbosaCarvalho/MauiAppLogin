cb49bedb6409a9867e97857fc7e1d0bd79ea0e0d
CristhianBarbosaCarvalho <crizin9@gmail.com>
2025-05-03 18:02:28 -0300

    finalizaçâo do projeto login

---
+++ MauiAppLogin/DadosUsuarios.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppLogin
+{
+    public class DadosUsuarios
+    {
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -6,4 +6,51 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+
+			List<DadosUsuarios> Lista_Usuarios = new List<DadosUsuarios>()
+			{
+				new DadosUsuarios()
+				{
+					Usuario = "cristhian",
+					Senha = "1234"
+				},
+
+                new DadosUsuarios()
+                {
+                    Usuario = "marcos",
+                    Senha = "4321"
+                }
+
+            };
+
+			DadosUsuarios dados_digitados = new DadosUsuarios()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+			if (Lista_Usuarios.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha)))
+			{
+
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+				App.Current.MainPage = new Protegida();
+
+			}
+			else 
+			{
+				throw new Exception("Usuario ou Senha Invalidos");
+			}
+
+		}
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+		}
+    }
 }
